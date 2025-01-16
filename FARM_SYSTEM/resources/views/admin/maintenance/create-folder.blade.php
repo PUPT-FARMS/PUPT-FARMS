89d3c237e9c78fdd0f25b85b081c4c3f07bdd712
Diana Rose Fidel <fideldianarose@gmail.com>
2025-01-16 16:52:29 +0800

    add dashboard section

--- FARM_SYSTEM/resources/views/admin/maintenance/create-folder.blade.php
+++ FARM_SYSTEM/resources/views/admin/maintenance/create-folder.blade.php
@@ -75,3 +75,232 @@
     </div>
 </body>
 </html>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Manage Main Requirements</title>
+    <link rel="icon" href="{{ asset('assets/images/pup-logo.png') }}" type="image/x-icon">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../../../../asset/vendor/bootstrap/css/bootstrap.min.css">
+    <link href="../../../../asset/vendor/fonts/circular-std/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../../../asset/libs/css/style.css">
+    <link rel="stylesheet" href="../../../../asset/vendor/fonts/fontawesome/css/fontawesome-all.css">
+    <link rel="stylesheet" type="text/css" href="../../../../asset/vendor/datatables/css/dataTables.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../../../../asset/vendor/datatables/css/buttons.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../../../../asset/vendor/datatables/css/select.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../../../../asset/vendor/datatables/css/fixedHeader.bootstrap4.css">
+</head>
+
+<body>
+    @include('partials.admin-sidebar')
+    <div id="loading-spinner" class="loading-spinner">
+        <div class="spinner"></div>
+    </div>
+    <div class="dashboard-wrapper">
+        <div class="dashboard-ecommerce">
+            <div class="container-fluid dashboard-content ">
+                <!-- ============================================================== -->
+                <!-- pageheader  -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <div class="page-header">
+                            <h2 class="pageheader-title">Create Folder</h2>
+                            <div class="page-breadcrumb">
+                                <nav aria-label="breadcrumb">
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="#!"
+                                                class="breadcrumb-link">Maintenance</a></li>
+                                        <li class="breadcrumb-item"><a
+                                                href="{{ route('admin.maintenance.create-folder') }}"
+                                                class="breadcrumb-link" style="color: #3d405c;">Create Folder</a></li>
+                                    </ol>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ============================================================== -->
+                <!-- end pageheader  -->
+                <!-- ============================================================== -->
+
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <button type="button" class="btn btn-success mb-3" data-toggle="modal"
+                                    data-target="#addFolderModal">
+                                    <i class="fas fa-plus"></i> Add Folder
+                                </button>
+                                </a>
+                                @if (session('success'))
+                                    <div id="success-alert"
+                                        class="alert alert-success alert-dismissible fade show text-center"
+                                        role="alert">
+                                        {{ session('success') }}
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                @endif
+
+                                @if ($errors->any())
+                                    <div class="alert alert-danger alert-dismissible fade show text-center"
+                                        role="alert">
+                                        @foreach ($errors->all() as $error)
+                                            <div>{{ $error }}</div>
+                                        @endforeach
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                @endif
+
+                                <div class="modal fade" id="addFolderModal" tabindex="-1" role="dialog"
+                                    aria-labelledby="addFolderModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="addFolderModalLabel">Add Sub Folder</h5>
+                                                <button type="button" class="close" data-dismiss="modal"
+                                                    aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <form action="{{ route('admin.maintenance.store-folder') }}" method="POST">
+                                                @csrf
+                                                <div class="modal-body">
+                                                    <div class="form-group">
+                                                        <label for="folderName">Folder Name</label>
+                                                        <input type="text" class="form-control" id="folderName"
+                                                            name="folder_name" required>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="mainFolderName">Main Folder Name</label>
+                                                        <select class="form-control" id="mainFolderName"
+                                                            name="main_folder_name" required>
+                                                            <option value="Classroom Management">Classroom Management
+                                                            </option>
+                                                            <option value="Test Administration">Test Administration
+                                                            </option>
+                                                            <option value="Syllabus Preparation ">Syllabus Preparation
+                                                            </option>
+                                                        </select>
+                                                        <i class="fas fa-chevron-down position-absolute"
+                                            style="right: 25px; top: 75%; transform: translateY(-50%); pointer-events: none;"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-dismiss="modal">Close</button>
+                                                    <button type="submit" class="btn btn-primary">Save
+                                                        Folder</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="table-responsive">
+                                    <table class="table table-striped table-bordered first">
+                                        <thead>
+                                            <tr>
+                                                <th>No.</th>
+                                                <th>Folder Name</th>
+                                                <th>Main Folder Name</th>
+                                                <th>Actions</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($folders as $index => $folder)
+                                                <tr>
+                                                    <td>{{ $index + 1 }}</td>
+                                                    <td>{{ $folder->folder_name }}</td>
+                                                    <td>{{ $folder->main_folder_name }}</td>
+                                                    <td>
+                                                        {{-- <a href="{{ route('admin.maintenance.view-file-input', ['folder_input_id' => $folder->folder_name_id]) }}"
+                                                            class="btn btn-info btn-sm">
+                                                            View
+                                                        </a> --}}
+                                                        <button type="button" class="btn btn-warning btn-sm"
+                                                            data-toggle="modal" data-target="#editFolderModal"
+                                                            data-id="{{ $folder->folder_name_id }}"
+                                                            data-name="{{ $folder->folder_name }}"
+                                                            data-main-folder-name="{{ $folder->main_folder_name }}">
+                                                            Edit
+                                                        </button>
+                                                        <button type="button" class="btn btn-danger btn-sm"
+                                                            onclick="confirmDelete({{ $folder->folder_name_id }})">Delete</button>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+
+
+                                <!-- Edit Folder Modal -->
+                                <div class="modal fade" id="editFolderModal" tabindex="-1" role="dialog"
+                                    aria-labelledby="editFolderModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="editFolderModalLabel">Edit Folder</h5>
+                                                <button type="button" class="close" data-dismiss="modal"
+                                                    aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <form method="POST" action="">
+                                                @csrf
+                                                @method('PUT')
+                                                <div class="modal-body">
+                                                    <input type="hidden" id="editFolderId" name="folder_name_id">
+                                                    <div class="form-group">
+                                                        <label for="editFolderName">Folder Name</label>
+                                                        <input type="text" class="form-control"
+                                                            id="editFolderName" name="folder_name" required>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="editMainFolderName">Main Folder Name</label>
+                                                        <select class="form-control" id="editMainFolderName"
+                                                            name="main_folder_name" required>
+                                                            <option value="Classroom Management">Classroom Management
+                                                            </option>
+                                                            <option value="Test Administration">Test Administration
+                                                            </option>
+                                                            <option value="Syllabus Preparation">Syllabus Preparation
+                                                            </option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-dismiss="modal">Close</button>
+                                                    <button type="submit" class="btn btn-primary">Save
+                                                        changes</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- ============================================================== -->
+            <!-- end wrapper  -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- ============================================================== -->
+        <!-- end main wrapper  -->
+        <!-- ============================================================== -->
+</body>
+</html>
