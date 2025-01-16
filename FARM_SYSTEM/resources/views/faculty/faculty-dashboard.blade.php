5a6d0ea66c096ef2885505d5298888a197dff3bf
Kazel Villmarzo <kazelvillamarzo@gmail.com>
2025-01-16 21:15:58 +0800

    add some change in faculty dashboard

--- FARM_SYSTEM/resources/views/faculty/faculty-dashboard.blade.php
+++ FARM_SYSTEM/resources/views/faculty/faculty-dashboard.blade.php
@@ -2,7 +2,18 @@
 <html lang="en">
 
 <head>
+    <!-- Required meta tags -->
     @include('partials.faculty-header')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <title>Dashboard</title>
+    <style>
+        .icon {
+            font-size: 30px;
+            color: #800000;
+        }
+    </style>
 </head>
 
 <body>
