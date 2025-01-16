ca3897b38810df39f8ce4220088720b455c53d9d
Diana Rose Fidel <fideldianarose@gmail.com>
2025-01-16 16:26:01 +0800

    add partials admin sidebar

--- FARM_SYSTEM/resources/views/admin/announcement/admin-announcement.blade.php
+++ FARM_SYSTEM/resources/views/admin/announcement/admin-announcement.blade.php
@@ -34,3 +34,30 @@
 <body>
 </body>
 </html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    @include('partials.admin-header')
+
+
+    <title>Announcement</title>
+    <style>
+        .toggle-dropdown {
+            position: absolute;
+            margin-left: 20px;
+            top: 100%;
+            transform: translateX(-100%);
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            background-color: white;
+            border-radius: 4px;
+            z-index: 1000;
+        }
+    </style>
+</head>
+
+<body>
+    @include('partials.admin-sidebar')
+</body>
+</html>
