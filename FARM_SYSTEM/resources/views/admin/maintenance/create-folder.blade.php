de88ee9549ff32651f79dbda3bb9c7209f1eee62
Diana Rose Fidel <fideldianarose@gmail.com>
2025-01-16 16:52:00 +0800

    add loading spinner

--- FARM_SYSTEM/resources/views/admin/maintenance/create-folder.blade.php
+++ FARM_SYSTEM/resources/views/admin/maintenance/create-folder.blade.php
@@ -47,3 +47,31 @@
     @include('partials.admin-sidebar')
 </body>
 </html>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Manage Main Requirements</title>
+    <link rel="icon" href="{{ asset('assets/images/pup-logo.png') }}" type="image/x-icon">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../../../../asset/vendor/bootstrap/css/bootstrap.min.css">
+    <link href="../../../../asset/vendor/fonts/circular-std/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../../../asset/libs/css/style.css">
+    <link rel="stylesheet" href="../../../../asset/vendor/fonts/fontawesome/css/fontawesome-all.css">
+    <link rel="stylesheet" type="text/css" href="../../../../asset/vendor/datatables/css/dataTables.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../../../../asset/vendor/datatables/css/buttons.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../../../../asset/vendor/datatables/css/select.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../../../../asset/vendor/datatables/css/fixedHeader.bootstrap4.css">
+</head>
+
+<body>
+    @include('partials.admin-sidebar')
+    <div id="loading-spinner" class="loading-spinner">
+        <div class="spinner"></div>
+    </div>
+</body>
+</html>
