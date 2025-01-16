c1398ebbe9ea15d6d2ddf07033cc361673e3d0c6
Diana Rose Fidel <fideldianarose@gmail.com>
2025-01-16 15:45:43 +0800

    add loading spinner

--- FARM_SYSTEM/resources/views/admin/accomplishment/main-folder.blade.php
+++ FARM_SYSTEM/resources/views/admin/accomplishment/main-folder.blade.php
@@ -101,3 +101,55 @@
     @include('partials.admin-sidebar')
 </body>
 </html>
+@include('partials.tables-header')
+<title>Main Requirements</title>
+</head>
+<style>
+    .form-group {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+    }
+    
+    .col-form-label {
+        margin-right: 10px;
+        white-space: nowrap;
+    }
+
+    .col-sm-3 {
+        flex: 1;
+        min-width: 200px;
+        position: relative;
+    }
+
+    select.form-control {
+        width: 100%;
+        padding-right: 30px;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+    }
+
+    @media (max-width: 576px) {
+        .form-group {
+            flex-direction: column;
+            align-items: stretch;
+        }
+
+        .col-form-label {
+            margin-bottom: 5px;
+        }
+
+        .col-sm-3 {
+            width: 100%;
+        }
+    }
+</style>
+
+<body>
+    @include('partials.admin-sidebar')
+    <div id="loading-spinner" class="loading-spinner">
+        <div class="spinner"></div>
+    </div>
+</body>
+</html>
