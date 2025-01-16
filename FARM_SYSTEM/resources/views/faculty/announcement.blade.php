4c58c1e97f61fabc7a72e9d502ed0109d516cd2e
Kazel Villmarzo <kazelvillamarzo@gmail.com>
2025-01-16 20:57:52 +0800

    Add codes to announcement balde

---
+++ FARM_SYSTEM/resources/views/faculty/announcement.blade.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Archive</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../../../../asset/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="icon" href="{{ asset('assets/images/pup-logo.png') }}" type="image/x-icon">
+    <link href="../../../../asset/vendor/fonts/circular-std/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../../../asset/libs/css/style.css">
+    <link rel="stylesheet" href="../../../../asset/vendor/fonts/fontawesome/css/fontawesome-all.css">
+    <link rel="stylesheet" type="text/css" href="../../../../asset/vendor/datatables/css/dataTables.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../../../../asset/vendor/datatables/css/buttons.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../../../../asset/vendor/datatables/css/select.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../../../../asset/vendor/datatables/css/fixedHeader.bootstrap4.css">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <style>
+        .body-modal {
+            max-height: 480px;
+            overflow-y: auto;
+        }
+
+        .view-modal {
+            max-height: 400px;
+            overflow-y: auto;
+        }
+
+        .bordered-file-input {
+            border: 1px solid #ced4da;
+            border-radius: 0.25rem;
+            padding: 0.375rem 0.75rem;
+            background-color: #fff;
+            box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+        }
+
+        .modal-dialog {
+            max-width: 600px;
+        }
+
+        .form-group {
+            margin-bottom: 1rem;
+        }
+
+        p {
+            color: #3d405c;
+        }
+
+        strong {
+            color: rgb(27, 27, 27);
+        }
+    </style>
+</head>
+
+<body>
+</body>
+
+</html>
