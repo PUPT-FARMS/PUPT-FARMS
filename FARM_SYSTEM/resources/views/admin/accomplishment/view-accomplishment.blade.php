b180b58103ea82967f09a407237e7c4ab63e268c
Diana Rose Fidel <fideldianarose@gmail.com>
2025-01-16 16:05:13 +0800

    add dashboard section

--- FARM_SYSTEM/resources/views/admin/accomplishment/view-accomplishment.blade.php
+++ FARM_SYSTEM/resources/views/admin/accomplishment/view-accomplishment.blade.php
@@ -84,3 +84,69 @@
         <div class="spinner"></div>
     </div>
 </body>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>View Uploaded Files</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../../../../../asset/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="icon" href="{{ asset('assets/images/pup-logo.png') }}" type="image/x-icon">
+    <link href="../../../../../asset/vendor/fonts/circular-std/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../../../../asset/libs/css/style.css">
+    <link rel="stylesheet" href="../../../../../asset/vendor/fonts/fontawesome/css/fontawesome-all.css">
+    <link rel="stylesheet" type="text/css" href="../../../../../asset/vendor/datatables/css/dataTables.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../../../../../asset/vendor/datatables/css/buttons.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../../../../../asset/vendor/datatables/css/select.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../../../../../asset/vendor/datatables/css/fixedHeader.bootstrap4.css">
+</head>
+
+<body>
+    @include('partials.admin-sidebar')
+    <div id="loading-spinner" class="loading-spinner">
+        <div class="spinner"></div>
+    </div>
+
+    <div class="dashboard-wrapper">
+        <div class="dashboard-ecommerce">
+            <div class="container-fluid dashboard-content ">
+                <!-- ============================================================== -->
+                <!-- pageheader  -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <div class="page-header">
+                            <h2 class="pageheader-title">View Uploaded Files</h2>
+                            <div class="page-breadcrumb">
+                                <nav aria-label="breadcrumb">
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item">
+                                            <a href="{{ route('admin.accomplishment.department', ['folder_name_id' => $folder_name_id]) }}"
+                                                class="breadcrumb-link">
+                                                Department
+                                            </a>
+                                        </li>
+                                        <li class="breadcrumb-item"><a
+                                                href="{{ route('viewAccomplishmentDepartment', [
+                                                    'department' => urlencode($department),
+                                                    'folder_name_id' => $folder->folder_name_id,
+                                                ]) }}"
+                                                class="breadcrumb-link">Faculty</a></li>
+                                        <li class="breadcrumb-item"><a
+                                                href="{{ route('admin.accomplishment.view-accomplishment', [
+                                                    'user_login_id' => $faculty->user_login_id,
+                                                    'folder_name_id' => $folder->folder_name_id,
+                                                ]) }}"
+                                                class="breadcrumb-link" style=" color: #3d405c;">View Uploaded Files</a>
+                                        </li>
+                                    </ol>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+</body>
