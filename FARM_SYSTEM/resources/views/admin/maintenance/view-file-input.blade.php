d2c04c4fd62d10d6f4d12cb7b56052eb2a22d8cf
Diana Rose Fidel <fideldianarose@gmail.com>
2025-01-16 17:03:15 +0800

    add dashboard section

--- FARM_SYSTEM/resources/views/admin/maintenance/view-file-input.blade.php
+++ FARM_SYSTEM/resources/views/admin/maintenance/view-file-input.blade.php
@@ -75,3 +75,65 @@
     </div>
 </body>
 </html>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Manage Main Requirements</title>
+    <link rel="icon" href="{{ asset('assets/images/pup-logo.png') }}" type="image/x-icon">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../../../../asset/vendor/bootstrap/css/bootstrap.min.css">
+    <link href="../../../../asset/vendor/fonts/circular-std/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../../../asset/libs/css/style.css">
+    <link rel="stylesheet" href="../../../../asset/vendor/fonts/fontawesome/css/fontawesome-all.css">
+    <link rel="stylesheet" type="text/css" href="../../../../asset/vendor/datatables/css/dataTables.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../../../../asset/vendor/datatables/css/buttons.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../../../../asset/vendor/datatables/css/select.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../../../../asset/vendor/datatables/css/fixedHeader.bootstrap4.css">
+</head>
+
+<body>
+    @include('partials.admin-sidebar')
+    <div id="loading-spinner" class="loading-spinner">
+        <div class="spinner"></div>
+    </div>
+    <div class="dashboard-wrapper">
+        <div class="dashboard-ecommerce">
+            <div class="container-fluid dashboard-content ">
+                <!-- ============================================================== -->
+                <!-- pageheader  -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <div class="page-header">
+                            <h2 class="pageheader-title">Add Input Field</h2>
+                            <div class="page-breadcrumb">
+                                <nav aria-label="breadcrumb">
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="#!" class="breadcrumb-link"
+                                                style="cursor: default; color: #3d405c;">Maintenance</a></li>
+                                        <li class="breadcrumb-item"><a
+                                                href="{{ route('admin.maintenance.create-folder') }}"
+                                                class="breadcrumb-link">Create Folder</a></li>
+                                        <li class="breadcrumb-item">
+                                            <a href="{{ route('admin.maintenance.view-file-input', ['folder_input_id' => $folder->folder_name_id]) }}"
+                                                class="breadcrumb-link">
+                                                View Input Field
+                                            </a>
+                                        </li>
+
+                                    </ol>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ============================================================== -->
+                <!-- end pageheader  -->
+                <!-- ============================================================== -->
+</body>
+</html>
