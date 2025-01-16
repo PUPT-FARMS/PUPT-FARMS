a3075995648e3cfdf62ded7200284bf3668d9f21
Kazel Villmarzo <kazelvillamarzo@gmail.com>
2025-01-16 22:03:36 +0800

    add src link

--- FARM_SYSTEM/resources/views/partials/director-header.blade.php
+++ FARM_SYSTEM/resources/views/partials/director-header.blade.php
@@ -14,3 +14,4 @@
 <link rel="stylesheet" href="../../../../asset/vendor/charts/c3charts/c3.css">
 <link rel="stylesheet" href="../../../../asset/vendor/fonts/flag-icon-css/flag-icon.min.css">
 <link rel="stylesheet" href="../../../../asset/libs/css/header-responsive.css">
+<script src="https://cdn.ckeditor.com/ckeditor5/35.3.0/classic/ckeditor.js"></script>
