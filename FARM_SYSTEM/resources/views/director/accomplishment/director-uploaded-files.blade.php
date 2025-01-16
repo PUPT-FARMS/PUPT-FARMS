5158bcb8c52c41d56f7980cc2a807373b4cb52ff
Diana Rose Fidel <fideldianarose@gmail.com>
2025-01-16 17:47:51 +0800

    add partials director sidebar and title

--- FARM_SYSTEM/resources/views/director/accomplishment/director-uploaded-files.blade.php
+++ FARM_SYSTEM/resources/views/director/accomplishment/director-uploaded-files.blade.php
@@ -1,1 +1,10 @@
 @include('partials.tables-header')
+@include('partials.tables-header')
+<title>{{ $folderName }} </title>
+</head>
+
+<body>
+    @include('partials.director-sidebar')
+</body>
+
+</html>
