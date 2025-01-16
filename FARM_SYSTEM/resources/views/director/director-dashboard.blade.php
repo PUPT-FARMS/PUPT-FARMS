e9fbc245eca308cb04c4791e28bdeae195bef1cf
Diana Rose Fidel <fideldianarose@gmail.com>
2025-01-16 18:03:19 +0800

    add loading spinner

--- FARM_SYSTEM/resources/views/director/director-dashboard.blade.php
+++ FARM_SYSTEM/resources/views/director/director-dashboard.blade.php
@@ -70,3 +70,36 @@
 </body>
 
 </html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    @include('partials.director-header')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <title>Dashboard</title>
+    <style>
+        .icon {
+            font-size: 30px;
+            color: #800000;
+        }
+
+        .small-chart {
+            width: 50vw;
+            height: 50vw;
+            max-width: 400px;
+            max-height: 400px;
+            display: block;
+        }
+    </style>
+</head>
+
+<body>
+    @include('partials.director-sidebar')
+    <div id="loading-spinner" class="loading-spinner">
+        <div class="spinner"></div>
+    </div>
+</body>
+</html>
