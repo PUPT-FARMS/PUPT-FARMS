eec4d861d6e56b39f7e8334f6e0f5c15e2ec31c9
Kazel Villmarzo <kazelvillamarzo@gmail.com>
2025-01-16 22:15:48 +0800

    add src link

--- FARM_SYSTEM/resources/views/partials/faculty-header.blade.php
+++ FARM_SYSTEM/resources/views/partials/faculty-header.blade.php
@@ -13,3 +13,4 @@
     href="../../../../asset/vendor/fonts/material-design-iconic-font/css/materialdesignicons.min.css">
 <link rel="stylesheet" href="../../../../asset/vendor/charts/c3charts/c3.css">
 <link rel="stylesheet" href="../../../../asset/vendor/fonts/flag-icon-css/flag-icon.min.css">
+<script src="https://cdn.ckeditor.com/ckeditor5/35.3.0/classic/ckeditor.js"></script>
