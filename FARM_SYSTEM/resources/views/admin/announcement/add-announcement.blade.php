8c482f8b26315a6975c6cec75127a774f5200644
Diana Rose Fidel <fideldianarose@gmail.com>
2025-01-16 16:20:53 +0800

    add partials admin sidebar

--- FARM_SYSTEM/resources/views/admin/announcement/add-announcement.blade.php
+++ FARM_SYSTEM/resources/views/admin/announcement/add-announcement.blade.php
@@ -24,3 +24,19 @@
 <body>
 </body>
 </html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    @include('partials.admin-header')
+    <link rel="stylesheet" href="../../../../asset/vendor/select2/css/select2.css">
+    <link rel="stylesheet" href="../../../../asset/vendor/summernote/css/summernote-bs4.css">
+    <title>Announcement</title>
+
+</head>
+
+<body>
+    @include('partials.admin-sidebar')
+</body>
+</html>
