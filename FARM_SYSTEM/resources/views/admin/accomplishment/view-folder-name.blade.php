94859f5108aa7c2c92fe83de13bed9034accb0b4
Diana Rose Fidel <fideldianarose@gmail.com>
2025-01-16 16:11:35 +0800

    add title and styling

--- FARM_SYSTEM/resources/views/admin/accomplishment/view-folder-name.blade.php
+++ FARM_SYSTEM/resources/views/admin/accomplishment/view-folder-name.blade.php
@@ -1,1 +1,45 @@
 @include('partials.tables-header')
+@include('partials.tables-header')
+<title>Main Requirements</title>
+</head>
+<style>
+    .form-group {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+    }
+
+    .col-form-label {
+        margin-right: 10px;
+        white-space: nowrap;
+    }
+
+    .col-sm-3 {
+        flex: 1;
+        min-width: 200px;
+        position: relative;
+    }
+
+    select.form-control {
+        width: 100%;
+        padding-right: 30px;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+    }
+
+    @media (max-width: 576px) {
+        .form-group {
+            flex-direction: column;
+            align-items: stretch;
+        }
+
+        .col-form-label {
+            margin-bottom: 5px;
+        }
+
+        .col-sm-3 {
+            width: 100%;
+        }
+    }
+</style>
