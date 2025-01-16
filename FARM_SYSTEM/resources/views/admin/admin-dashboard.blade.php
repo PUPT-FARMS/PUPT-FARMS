a325563e644cdab624eb4017f72d3a4457ada2d0
Diana Rose Fidel <fideldianarose@gmail.com>
2025-01-16 17:21:55 +0800

    add partials admin header and styles

--- FARM_SYSTEM/resources/views/admin/admin-dashboard.blade.php
+++ FARM_SYSTEM/resources/views/admin/admin-dashboard.blade.php
@@ -7,3 +7,31 @@
 <body>
 </body>
 </html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    @include('partials.admin-header')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <title>Dashboard</title>
+    <style>
+        .icon {
+            font-size: 30px;
+            color: #800000;
+        }
+
+        .small-chart {
+            width: 50vw;
+            height: 50vw;
+            max-width: 400px;
+            max-height: 400px;
+            display: block;
+        }
+    </style>
+</head>
+
+<body>
+</body>
+</html>
