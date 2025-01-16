ca45d4361935f987f5f53d2fbf59551b1ddfaceb
Diana Rose Fidel <fideldianarose@gmail.com>
2025-01-16 16:31:49 +0800

    add required header tags and title

---
+++ FARM_SYSTEM/resources/views/admin/announcement/edit-announcement.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <link rel="stylesheet" href="../../../../asset/vendor/select2/css/select2.css">
+    <link rel="stylesheet" href="../../../../asset/vendor/summernote/css/summernote-bs4.css">
+    <title>Announcement</title>
+</head>
+
+<body>
+</body>
+
+</html>
