b4ae48c3496aa13687dc6e9fac3039537b541026
Diana Rose Fidel <fideldianarose@gmail.com>
2025-01-16 16:04:07 +0800

    add partials admin sidebar

--- FARM_SYSTEM/resources/views/admin/accomplishment/view-accomplishment.blade.php
+++ FARM_SYSTEM/resources/views/admin/accomplishment/view-accomplishment.blade.php
@@ -32,3 +32,28 @@
 <body>
 </body>
 </html>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>View Uploaded Files</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../../../../../asset/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="icon" href="{{ asset('assets/images/pup-logo.png') }}" type="image/x-icon">
+    <link href="../../../../../asset/vendor/fonts/circular-std/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../../../../asset/libs/css/style.css">
+    <link rel="stylesheet" href="../../../../../asset/vendor/fonts/fontawesome/css/fontawesome-all.css">
+    <link rel="stylesheet" type="text/css" href="../../../../../asset/vendor/datatables/css/dataTables.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../../../../../asset/vendor/datatables/css/buttons.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../../../../../asset/vendor/datatables/css/select.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="../../../../../asset/vendor/datatables/css/fixedHeader.bootstrap4.css">
+</head>
+
+<body>
+    @include('partials.admin-sidebar')
+</body>
+</html>
