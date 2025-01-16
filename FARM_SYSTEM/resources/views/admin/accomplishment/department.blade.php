496b5841d38b19c39d411f8622007da9fe82f7ba
Diana Rose Fidel <fideldianarose@gmail.com>
2025-01-16 15:35:50 +0800

    add loading spinner

--- FARM_SYSTEM/resources/views/admin/accomplishment/department.blade.php
+++ FARM_SYSTEM/resources/views/admin/accomplishment/department.blade.php
@@ -110,3 +110,64 @@
     @include('partials.admin-sidebar')
 </body>
 </html>
+@include('partials.tables-header')
+<title>{{ $folderName }} </title>
+</head>
+<style>
+    .form-group {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+    }
+
+    .col-form-label {
+        margin-right: 10px;
+        white-space: nowrap;
+    }
+
+    .col-sm-3 {
+        flex: 1;
+        min-width: 200px;
+        position: relative;
+    }
+
+    select.form-control {
+        width: 100%;
+        padding-right: 30px;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+    }
+
+    .fas.fa-chevron-down {
+        position: absolute;
+        top: 50%;
+        right: 25px;
+        transform: translateY(-50%);
+        pointer-events: none;
+        z-index: 1;
+    }
+
+    @media (max-width: 576px) {
+        .form-group {
+            flex-direction: column;
+            align-items: stretch;
+        }
+
+        .col-form-label {
+            margin-bottom: 5px;
+        }
+
+        .col-sm-3 {
+            width: 100%;
+        }
+    }
+</style>
+
+<body>
+    @include('partials.admin-sidebar')
+    <div id="loading-spinner" class="loading-spinner">
+        <div class="spinner"></div>
+    </div>
+</body>
+</html>
