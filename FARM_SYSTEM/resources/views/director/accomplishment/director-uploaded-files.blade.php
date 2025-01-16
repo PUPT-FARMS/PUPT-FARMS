902dd914125ca230b10988e8086f7411f57c9232
Diana Rose Fidel <fideldianarose@gmail.com>
2025-01-16 17:48:20 +0800

    add loading spinner

--- FARM_SYSTEM/resources/views/director/accomplishment/director-uploaded-files.blade.php
+++ FARM_SYSTEM/resources/views/director/accomplishment/director-uploaded-files.blade.php
@@ -8,3 +8,14 @@
 </body>
 
 </html>
+@include('partials.tables-header')
+<title>{{ $folderName }} </title>
+</head>
+
+<body>
+    @include('partials.director-sidebar')
+    <div id="loading-spinner" class="loading-spinner">
+        <div class="spinner"></div>
+    </div>
+</body>
+</html>
