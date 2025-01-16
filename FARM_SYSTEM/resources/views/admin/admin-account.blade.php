87c8823efd774b9925d2c784bbf12b7cf2aa587d
Diana Rose Fidel <fideldianarose@gmail.com>
2025-01-16 17:17:59 +0800

    add loading spinner

--- FARM_SYSTEM/resources/views/admin/admin-account.blade.php
+++ FARM_SYSTEM/resources/views/admin/admin-account.blade.php
@@ -56,3 +56,31 @@
 	@include('partials.admin-sidebar')
 </body>
 </html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    @include('partials.admin-header')
+    <title>View Account</title>
+    <style>
+        .toggle-dropdown {
+            position: absolute;
+            margin-left: 20px;
+            top: 100%;
+            transform: translateX(-100%);
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            background-color: white;
+            border-radius: 4px;
+            z-index: 1000;
+        }
+    </style>
+</head>
+
+<body>
+	@include('partials.admin-sidebar')
+	<div id="loading-spinner" class="loading-spinner">
+        <div class="spinner"></div>
+    </div>
+</body>
+</html>
