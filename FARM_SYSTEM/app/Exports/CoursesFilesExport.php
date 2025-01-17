c69cf8d8667cb8d244b4e7ff93ea9900d292bc46
nabayra-james <james.nabayra029@gmail.com>
2025-01-18 00:07:27 +0800

    import maatwebsite

---
+++ FARM_SYSTEM/app/Exports/CoursesFilesExport.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
