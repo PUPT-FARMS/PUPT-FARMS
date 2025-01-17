03b787c3c48dd31e96f1c06cc539cd51d002e91c
nabayra-james <james.nabayra029@gmail.com>
2025-01-17 22:26:16 +0800

    add user login id

--- FARM_SYSTEM/database/migrations/2025_01_17_135135_create_notifications_table.php
+++ FARM_SYSTEM/database/migrations/2025_01_17_135135_create_notifications_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('courses_files_id');
+            $table->unsignedBigInteger('user_login_id');
             $table->timestamps();
         });
     }
