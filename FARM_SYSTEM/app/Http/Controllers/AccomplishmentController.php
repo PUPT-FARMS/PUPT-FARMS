9551597d2ae85f58209973eb15795bb6422c3327
nabayra-james <james.nabayra029@gmail.com>
2025-01-18 00:22:37 +0800

    add accomplishment page function

--- FARM_SYSTEM/app/Http/Controllers/AccomplishmentController.php
+++ FARM_SYSTEM/app/Http/Controllers/AccomplishmentController.php
@@ -15,3 +15,39 @@ use App\Models\LoginLog;
 use App\Models\Department;
 use Carbon\Carbon;
 use App\Models\CourseSchedule;
+
+class AccomplishmentController extends Controller
+{
+    //show the accomplishtment department
+    public function showAccomplishmentPage()
+    {
+        if (!auth()->check()) {
+            return redirect()->route('login');
+        }
+    
+        $user = auth()->user();
+        $firstName = $user->first_name;
+        $surname = $user->surname;
+ 
+    
+        $notifications = Notification::where('user_login_id', $user->user_login_id)
+            ->orderBy('created_at', 'desc')
+            ->get();
+    
+        $notificationCount = $notifications->where('is_read', 0)->count();
+    
+        $folders = FolderName::all();
+    
+        $departments = Department::all();
+    
+        return view('admin.accomplishment.accomplishment', [
+            'folders' => $folders,
+            'notifications' => $notifications,
+            'notificationCount' => $notificationCount,
+            'firstName' => $firstName,
+            'surname' => $surname,
+            'user' => $user, 
+            'departments' => $departments,
+        ]);
+    }
+}
