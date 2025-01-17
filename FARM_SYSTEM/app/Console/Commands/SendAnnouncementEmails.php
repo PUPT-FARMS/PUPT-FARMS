5f756a8a02e26b5bf1e4f1989946f1ac2a18860c
nabayra-james <james.nabayra029@gmail.com>
2025-01-17 23:49:31 +0800

    add protected signature

--- FARM_SYSTEM/app/Console/Commands/SendAnnouncementEmails.php
+++ FARM_SYSTEM/app/Console/Commands/SendAnnouncementEmails.php
@@ -5,3 +5,14 @@ use Illuminate\Console\Command;
 use App\Models\Announcement;
 use Illuminate\Support\Facades\Mail;
 use App\Models\UserLogin;
+
+class SendAnnouncementEmails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send:announcement-emails {announcementId}';
+        
+}
