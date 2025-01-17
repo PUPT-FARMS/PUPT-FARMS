6bade6f9313e7b89a97b2a2423eef6fb98fe67a3
nabayra-james <james.nabayra029@gmail.com>
2025-01-17 22:30:51 +0800

    add boolean to is_read

--- FARM_SYSTEM/database/migrations/2025_01_17_135221_add_is_read_to_notifications_table.php
+++ FARM_SYSTEM/database/migrations/2025_01_17_135221_add_is_read_to_notifications_table.php
@@ -9,10 +9,10 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::table('notifications', function (Blueprint $table) {
-            //
+            $table->boolean('is_read')->default(false);
         });
     }
 
