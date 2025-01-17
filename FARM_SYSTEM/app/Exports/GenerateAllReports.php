b58796de870b46d92dda6d67a45a692dd9e51778
nabayra-james <james.nabayra029@gmail.com>
2025-01-18 00:15:10 +0800

    import maatwebsite and models

---
+++ FARM_SYSTEM/app/Exports/GenerateAllReports.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\CoursesFile;
+use App\Models\FolderName;
+use App\Models\UserLogin;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
