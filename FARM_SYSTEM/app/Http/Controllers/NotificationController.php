a53eb1095e046d81492dd1da34d144e65e7d9d9e
nabayra-james <james.nabayra029@gmail.com>
2025-01-18 00:25:17 +0800

    add foldername model

--- FARM_SYSTEM/app/Http/Controllers/NotificationController.php
+++ FARM_SYSTEM/app/Http/Controllers/NotificationController.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use App\Models\UserLogin;
+use App\Models\FolderName;
 use Illuminate\Support\Facades\Log; 
 
 class NotificationController extends Controller
