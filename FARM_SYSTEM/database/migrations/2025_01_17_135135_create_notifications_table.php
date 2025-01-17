8dee3e036b8b4c47302195afc0c0a03e21ec387e
nabayra-james <james.nabayra029@gmail.com>
2025-01-17 22:31:49 +0800

    remove comments

--- FARM_SYSTEM/database/migrations/2025_01_17_135135_create_notifications_table.php
+++ FARM_SYSTEM/database/migrations/2025_01_17_135135_create_notifications_table.php
@@ -6,10 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-        public function up()
+    public function up()
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
