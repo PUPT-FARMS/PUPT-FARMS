7c3fa2ef7e3167a6319688e1687ff547bdd0f2fc
nabayra-james <james.nabayra029@gmail.com>
2025-01-18 00:26:37 +0800

    add mark notif as read function

--- FARM_SYSTEM/app/Http/Controllers/NotificationController.php
+++ FARM_SYSTEM/app/Http/Controllers/NotificationController.php
@@ -21,4 +21,23 @@ class NotificationController extends Controller
                             ->count();
         return response()->json(['count' => $count]);
     }
+
+    public function markNotificationsAsRead(Request $request)
+    {
+        $notificationId = $request->input('notification_id');
+    
+        if ($notificationId) {
+            // Mark specific notification as read
+            Notification::where('id', $notificationId)
+                        ->where('user_login_id', Auth::id())
+                        ->update(['is_read' => true]);
+        } else {
+            // Mark all notifications as read
+            Notification::where('user_login_id', Auth::id())
+                        ->where('is_read', false)
+                        ->update(['is_read' => true]);
+        }
+    
+        return response()->json(['status' => 'success']);
+    }
 }
