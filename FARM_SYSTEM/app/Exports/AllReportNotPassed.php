4dd9af7930a33c1356465a3310098ff038511716
nabayra-james <james.nabayra029@gmail.com>
2025-01-18 00:06:42 +0800

    add function to get faculty info

--- FARM_SYSTEM/app/Exports/AllReportNotPassed.php
+++ FARM_SYSTEM/app/Exports/AllReportNotPassed.php
@@ -91,4 +91,99 @@ class AllReportNotPassed implements FromCollection, WithHeadings, WithEvents, Sh
         ];
     }
 
+    private function getFacultyInfo()
+    {
+        $semester = [
+            "id" => 1,
+            "semester" => "1st Semester 2024-2025",
+            "user_login_id" => 2
+        ];
+    
+        $programs = [
+            "Bachelor of Science in Applied Mathematics (BSAM)",
+            "Bachelor of Science in Information Technology (BSIT)",
+            "Bachelor of Science in Entrepreneurship (BSENTREP)"
+        ];
+    
+        $subjects = [
+            [
+                "id" => 1,
+                "code" => "MGT101",
+                "name" => "Principles of Management and Organization",
+                "semester" => $semester,
+                "year_programs" => [
+                    [
+                        "year" => "1st Year",
+                        "program" => "BSAM"
+                    ]
+                ]
+            ],
+            [
+                "id" => 2,
+                "code" => "IT202",
+                "name" => "Applications Development and Emerging Technologies",
+                "semester" => $semester,
+                "year_programs" => [
+                    [
+                        "year" => "2nd Year",
+                        "program" => "BSIT"
+                    ]
+                ]
+            ],
+            [
+                "id" => 3,
+                "code" => "ENT301",
+                "name" => "Technopreneurship",
+                "semester" => $semester,
+                "year_programs" => [
+                    [
+                        "year" => "3rd Year",
+                        "program" => "BSENTREP"
+                    ]
+                ]
+            ],
+            [
+                "id" => 4,
+                "code" => "SYS202",
+                "name" => "Systems Analysis and Design",
+                "semester" => $semester,
+                "year_programs" => [
+                    [
+                        "year" => "2nd Year",
+                        "program" => "BSIT"
+                    ]
+                ]
+            ],
+            [
+                "id" => 5,
+                "code" => "CS303",
+                "name" => "Computer Science",
+                "semester" => $semester,
+                "year_programs" => [
+                    [
+                        "year" => "4th Year",
+                        "program" => "BSIT"
+                    ],
+                    [
+                        "year" => "3rd Year",
+                        "program" => "BSAM"
+                    ]
+                ]
+            ]
+        ];
+    
+        $data = [
+            "faculty" => [
+                "faculty_id" => 2,
+                "first_name" => "Diana",
+                "middle_name" => "M.",
+                "last_name" => "Rose",
+                "programs" => $programs,
+                "subjects" => $subjects
+            ]
+        ];
+    
+        return $data;
+    }
+
 }
