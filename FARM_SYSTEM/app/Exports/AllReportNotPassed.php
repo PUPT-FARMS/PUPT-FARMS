16c308404543d86ca44c59fe11a5b3a7693d42d3
nabayra-james <james.nabayra029@gmail.com>
2025-01-17 23:56:23 +0800

    import maatwebsite

---
+++ FARM_SYSTEM/app/Exports/AllReportNotPassed.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+
+class AllReportNotPassed implements FromCollection, WithHeadings, WithEvents, ShouldAutoSize
+{
+
+}
