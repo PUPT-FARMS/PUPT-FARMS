c371456b983e06037a0ed0b6217152fc29559c32
nabayra-james <james.nabayra029@gmail.com>
2025-01-17 23:50:48 +0800

    add handle function

--- FARM_SYSTEM/app/Console/Commands/SendAnnouncementEmails.php
+++ FARM_SYSTEM/app/Console/Commands/SendAnnouncementEmails.php
@@ -21,5 +21,33 @@ class SendAnnouncementEmails extends Command
      * @var string
      */
     protected $description = 'Send announcement emails to the specified recipients';
-    
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $announcementId = $this->argument('announcementId');
+        $announcement = Announcement::find($announcementId);
+
+        if (!$announcement) {
+            $this->error('Announcement not found.');
+            return 1;
+        }
+
+        $recipientEmailsString = $announcement->type_of_recepient;
+
+        if ($recipientEmailsString === 'All Faculty') {
+            $recipients = User::where('role', 'faculty')->pluck('email')->toArray();
+        } else {
+            $recipients = explode(', ', $recipientEmailsString);
+        }
+
+        $this->sendAnnouncementEmails($announcement, $recipients);
+
+        $this->info('Emails have been sent successfully.');
+        return 0;
+    }
 }
